4f7e5da195b0d9778bbcf90396be1aa365626aab
jay sheth <jsheth066@gmail.com>
2026-08-06 10:26:40 +0400

    ADD: application Table

---
+++ Tables/Application.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[Application]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Uuid] UNIQUEIDENTIFIER NOT NULL, 
+    [Name] NVARCHAR(200) NOT NULL, 
+    [DisplayName] NVARCHAR(200) NULL, 
+    [Description] NVARCHAR(4000) NULL,
+    [Version] NVARCHAR(200) NOT NULL, 
+    [CreatedAt] DATETIMEOFFSET NOT NULL, 
+    [CreatedBy] INT NULL, 
+    [UpdatedAt] DATETIMEOFFSET NULL, 
+    [UpdatedBy] INT NULL, 
+    [DeletedAt] DATETIMEOFFSET NULL ,
+    [DeletedBy] INT NULL
+)
+
+
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Application_Uuid] ON [dbo].[Application] (Uuid)
+
+--GO
+
+--CREATE CLUSTERED INDEX [IX_Application_Id] ON [dbo].[Application] (Id)
