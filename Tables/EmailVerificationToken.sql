3b3e539da052f89940fcd2039eaa77a0a1710d6f
jay sheth <jsheth066@gmail.com>
2026-08-06 10:27:00 +0400

    ADD: emailVerifiaction Table

---
+++ Tables/EmailVerificationToken.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE [dbo].EmailVerificationToken
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Uuid] UNIQUEIDENTIFIER NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [TokenHash] NVARCHAR(64) NOT NULL, 
+    [ExpiresAt] DATETIMEOFFSET NOT NULL,
+    [UsedAt] DATETIMEOFFSET NULL, 
+    [CreatedAt] DATETIMEOFFSET NOT NULL, 
+    [CreatedBy] INT NULL, 
+    [UpdatedAt] DATETIMEOFFSET NULL, 
+    [UpdatedBy] INT NULL, 
+    [DeletedAt] DATETIMEOFFSET NULL ,
+    [DeletedBy] INT NULL, 
+    CONSTRAINT [FK_EmailVerificationToken_User_UserId] FOREIGN KEY (UserId) REFERENCES [User](Id) 
+)
+
+
+
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_EmailVerificationToken_UserId] ON [dbo].[EmailVerificationToken] (UserId)
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_EmailVerificationToken_TokenHash] ON [dbo].[EmailVerificationToken] (TokenHash)
+
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_EmailVerificationToken_Uuid] ON [dbo].[EmailVerificationToken] (Uuid)
+
+--GO
+
+--CREATE CLUSTERED INDEX [IX_EmailVerificationToken_Id] ON [dbo].EmailVerificationToken (Id)
