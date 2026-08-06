56025300c94d32350c3c245046fc4982e878e490
jay sheth <jsheth066@gmail.com>
2026-08-06 10:37:16 +0400

    UPDATE: Enhance MetaObject table by adding ApplicationUid column and foreign key constraints, along with unique and nonclustered indexes for ObjUid, ObjTypeUid, and ApplicationUid.

--- Tables/MetaObject.sql
+++ Tables/MetaObject.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[MetaObject]
 (
     [Id] INT NOT NULL PRIMARY KEY,
     [ObjUid] UNIQUEIDENTIFIER NOT NULL, 
-    [ObjTypeUid] UNIQUEIDENTIFIER NOT NULL,
+    [ObjTypeUid] UNIQUEIDENTIFIER,
+    [ApplicationUid] UNIQUEIDENTIFIER NOT NULL, 
     [Name] NVARCHAR(200) NOT NULL, 
     [DisplayName] NVARCHAR(200) NULL, 
     [Description] NVARCHAR(4000) NULL,
@@ -14,4 +15,10 @@ CREATE TABLE [dbo].[MetaObject]
     [UpdatedBy] INT NULL, 
     [DeletedAt] DATETIMEOFFSET NULL ,
     [DeletedBy] INT NULL
+    CONSTRAINT [FK_MetaObject_Application_ApplicationUid] FOREIGN KEY (ApplicationUid) REFERENCES [Application](Uuid)
+    CONSTRAINT [FK_MetaObject_MetaObject_ObjTypeUid] FOREIGN KEY (ObjTypeUid) REFERENCES [MetaObject](ObjUid)
 )
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_MetaObject_ObjUid] ON [dbo].[MetaObject] (ObjUid)
+CREATE NONCLUSTERED INDEX [IX_MetaObject_ObjTypeUid] ON [dbo].[MetaObject] (ObjTypeUid)
+CREATE NONCLUSTERED INDEX [IX_MetaObject_ApplicationUid] ON [dbo].[MetaObject] (ApplicationUid)
